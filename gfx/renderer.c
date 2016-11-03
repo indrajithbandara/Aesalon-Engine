137c0adb7fda96250117c18c5fd1979072bcdc48
Brian Millar <amb3r@protonmail.ch>
2016-11-03 19:52:09 +0000

    Removed Legacy Copy Pasted Example Code

--- gfx/renderer.c
+++ gfx/renderer.c
@@ -2,13 +2,5 @@ void asln_render(void);
 
 void asln_render(void) {
     /*glClear(GL_COLOR_BUFFER_BIT);*/
-    glBegin(GL_TRIANGLES);
-        glColor3f(1, 0, 0);
-        glVertex3f(-1, 0, 0);
-        glColor3f(0, 1, 0);
-        glVertex3f(0, 1, 0);
-        glColor3f(0, 0, 1);
-        glVertex3f(1, 0, 0);
-    glEnd();
     glfwSwapBuffers(ASLN_WINDOW);
 }
